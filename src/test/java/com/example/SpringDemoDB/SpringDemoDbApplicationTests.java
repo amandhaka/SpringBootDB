651190dd42aab818896f93e87070b8e4513ff2c7
amandhaka <amandhaka2021@gmail.com>
2021-02-23 14:09:58 +0530

    Initial Commit

---
+++ src/test/java/com/example/SpringDemoDB/SpringDemoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
